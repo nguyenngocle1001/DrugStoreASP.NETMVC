bc0da76cc5261cc37c42b6000947548c1ec3fcda
Lexe <nguyenngocle1001@gmail.com>
2021-01-08 23:12:03 +0700

    start project

---
+++ DrugStore/Controllers/User/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DrugStore.Controllers.User
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Products()
+        {
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+        public ActionResult ProductsCategory()
+        {
+            return View();
+        }
+
+        public ActionResult ProductsManufacture()
+        {
+            return View();
+        }
+
+        public ActionResult Checkout()
+        {
+            return View();
+        }
+
+        public ActionResult Search()
+        {
+            return View();
+        }
+    }
+}
---
+++ DrugStore/Controllers/User/PartialController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DrugStore.Controllers.User
+{
+    public class PartialController : Controller
+    {
+        // GET: Partial
+        public ActionResult LeftSideBar()
+        {
+            return PartialView();
+        }
+
+        public ActionResult Categorys()
+        {
+            return PartialView();
+        }
+
+        public ActionResult Manufactures()
+        {
+            return PartialView();
+        }
+    }
+}
